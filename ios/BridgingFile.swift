285c0cc0c9feedcfd71c4f6e4445e5eaa746e7e0
Enzo Amorim <enzoamorim@Enzos-MacBook-Air.local>
2022-02-11 01:08:47 -0300

    first commit

---
+++ ios/BridgingFile.swift
@@ -0,0 +1,8 @@
+//
+//  BridgingFile.swift
+//  LiveStreamExample
+//
+//  Created by Enzo Amorim on 11/02/22.
+//
+
+import Foundation
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const SOCKET_IO_SERVER = 'http://localhost:3333';
+export const RTMP_SERVER = 'rtmp://localhost';
